3101e8919175a7b447f9b122e5eba469a0af82b4
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-05-24 13:44:22 +0200

    Initializing

---
+++ src/main/java/elisa_daria/be_mood_watch/BeMoodWatchApplication.java
@@ -0,0 +1,13 @@
+package elisa_daria.be_mood_watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMoodWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMoodWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/elisa_daria/be_mood_watch/BeMoodWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package elisa_daria.be_mood_watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMoodWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
