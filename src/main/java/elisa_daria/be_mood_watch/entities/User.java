43a7e7050e82f4081428f3a1c494de547a9699f4
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-05-25 11:31:11 +0200

    user-gets-authorities

--- src/main/java/elisa_daria/be_mood_watch/entities/User.java
+++ src/main/java/elisa_daria/be_mood_watch/entities/User.java
@@ -7,15 +7,19 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 @Entity
 @Table(name="users")
 @Getter
 @Setter
 @NoArgsConstructor
-@JsonIgnoreProperties({"reviews", "watchList"})
-public class User {
+@JsonIgnoreProperties({"reviews", "watchList","role","password","authorities", "accountNonExpired", "credentialsNonExpired", "accountNonLocked", "enabled"})
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
@@ -43,4 +47,28 @@ public class User {
         this.role = Role.USER;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority((this.role.name())));
+    }
 }
